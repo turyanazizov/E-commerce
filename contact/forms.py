b746f050315db7c555b511d1d1e983205b1e41b0
Turyan Azizov <turyanazizov13@gmail.com>
2022-04-05 13:27:32 +0400

    Contact page already ready

---
+++ contact/forms.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm
+from . models import Contact
+from django import forms
+from django.core.exceptions import ValidationError
+
+class ContactForm(ModelForm):
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Name'
+                }
+            ),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'E-mail'
+            }),
+            'message': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Message'
+            }),
+        }
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.urls import reverse
+from django.contrib import messages
 
 def contact(request):
-    return render(request,'contact.html')
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Your message has been received!')
+            return redirect(reverse('contact:contact'))
+            
+    return render(request, 'contact.html', {'form': form})
